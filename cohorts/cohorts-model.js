2071d6c9f05726007ad9d663dc1ff3f8b532a89a
Mychal Hall <hallmychal@gmail.com>
2019-05-29 14:07:18 -0500

    Custom Middleware written

--- cohorts/cohorts-model.js
+++ cohorts/cohorts-model.js
@@ -1,9 +1,16 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-  get
+  get,
+  getById
 };
 
 function get() {
   return db("cohorts");
+}
+
+function getById(id) {
+  return db("zoos")
+  .where({ id })
+  .first();
 }
--- cohorts/cohorts-router.js
+++ cohorts/cohorts-router.js
@@ -5,7 +5,7 @@ const Cohorts = require("./cohorts-model.js");
 
 const router = express.Router();
 
-
+// Lists all the zoos in the database == GET /api/cohorts
 router.get("/", async (req, res) => {
   try {
     const cohorts = await Cohorts.get();
@@ -15,5 +15,40 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: "Error getting cohorts." });
   }
 });
+
+// Returns one cohort with a specific ID -- GET /api/cohorts/:id
+router.get("/:id", validateCohortId)
+
+
+//  Middleware 
+
+async function validateCohortId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const zoo = await Cohorts.getById(id);
+    if (cohort) {
+      req.cohort = cohort;
+      next();
+    } else {
+      res.status(404).json({ message: "No Cohort exists with that ID" })
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Critical Error - Unable to comply." })
+  }
+}
+
+function validateCohort(req, res, next) {
+  if (req.body && Object.keys(req.body).length) {
+    if (req.body.name !== "") {
+      next();
+    } else {
+      res.status(400).json({ message: "Please name the Cohort" })
+    }
+  } else {
+    res.status(500).json({ messages: "Critical Error - Unable to comply." })
+  }
+}
+
 
 module.exports = router;
